9d8f132e621f1d2fd9408b032ca56cbbf946bd59
ncysatnaf <ncysatnaf@gmail.com>
2016-02-27 23:27:46 +0800

    23333

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,7 @@
 import fetch from 'isomorphic-fetch'
 
+
+//physical  --|====>  ))) interactions
 export const HANDLE_TOGGLE_CHANGE = 'HANDLE_TOGGLE_CHANGE'
 
 export function handleToggleChange(key, value) {
@@ -10,4 +12,41 @@ export function handleToggleChange(key, value) {
 	}
   }
 }
+
+//magical  ----(*  ~~~~  logic
+export const ADD_TODO = "ADD_TODO"
+export const EDIT_TODO = "EDIT_TODO"
+export const COMPLETE_TODO = "COMPLETE_TODO"
+
+export function addTodo(text) {
+	return {
+		type: ADD_TODO,
+		text
+	}
+}
+
+export function completeTodo(index) {
+	return {
+		type: COMPLETE_TODO,
+		index
+	}
+}
+
+//rule  >---->  ||| Vision
+export const SET_VISIBILITY_FILTER = "SET_VISIBILITY_FILTER"
+
+export function setVisibilityFilter(filter) {
+	return {
+		type: SET_VISIBILITY_FILTER,
+		filter
+	}
+}
+
+
+
+
+
+
+
+
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -12,8 +12,41 @@ function global(state = { isopen: false }, action) {
 	}
 }
 
+function todos(state = [],action) {
+	switch (action.type) {
+		case ADD_TODO:
+			return [...state, {
+				text: action.text,
+				completed: false
+			}]
+		case COMPLETE_TODO:
+			return [
+			...state.slice(0, action.index),
+			Object.assign({}, state[action.index], {
+				completed: true
+			}),
+			...state.slice(action.index + 1)
+			]
+		default:
+			return state
+	}
+}
+
+function visibilityFilter( state = SHOW_ALL, action) {
+	switch (action.type) {
+		case SET_VISIBILITY_FILTER:
+			return action.fliter
+		default:
+			return state
+	}
+}
+
+
+
 const rootReducer = combineReducers({
-	global
+	global,
+	todos,
+	visibilityFilter
 })
 
 export default rootReducer
